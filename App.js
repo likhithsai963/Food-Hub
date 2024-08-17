c1d6fdb9d7373bbec50b618ea2fe2268830cb1cb
likhithsai963 <likhithsai963@gmail.com>
2024-08-17 12:08:12 +0530

    react basic

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ *<div id="parent">
+    <div id="child">
+        <h1></h1>
+    </div>
+ </div>
+ */
+
+
+
+
+const parent = React.createElement(
+    "div",{id:"parent"},[
+    React.createElement(
+        "div",{id:"child"},
+        [React.createElement("h1",{},"I am a h1 tag"),
+            React.createElement("h2",{},"I am a h2 tag")]),
+        React.createElement(
+            "div",{id:"child2"},
+            [React.createElement("h1",{},"I am a h1 tag"),React.createElement("h2",{},"I am a h2 tag")])])
+
+
+const heading = React.createElement("h1",{id:"heading"},"Hello world from react");
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
